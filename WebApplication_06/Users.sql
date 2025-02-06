0536ac412a7a80ff2908c7053eb3ade1fc588861
Tung Le <lenhattung@gmail.com>
2025-02-06 12:53:32 +0700

    Update Users.sql

--- WebApplication_06/Users.sql
+++ WebApplication_06/Users.sql
@@ -1,10 +1,25 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-/**
- * Author:  tungi
- * Created: Feb 6, 2025
- */
+USE [master]
+GO
 
+CREATE DATABASE [prj301_1805_06]
+ 
+USE [prj301_1805_06]
+GO
+
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[tblUsers](
+	[userID] [varchar](50) NULL,
+	[fullName] [nvarchar](50) NULL,
+	[roleID] [char](2) NULL,
+	[password] [varchar](50) NULL
+) ON [PRIMARY]
+GO
+
+INSERT [dbo].[tblUsers] ([userID], [fullName], [roleID], [password]) VALUES (N'QL002', N'Administrator', N'AD', N'1')
+INSERT [dbo].[tblUsers] ([userID], [fullName], [roleID], [password]) VALUES (N'SE003', N'I am User', N'US', N'1')
+GO
