4ed0773e800b02e63ba6e87f194de97ac1d541cf
Tung Le <lenhattung@gmail.com>
2025-02-06 13:34:09 +0700

    update
    update

--- WebApplication_06/src/java/dto/UserDTO.java
+++ WebApplication_06/src/java/dto/UserDTO.java
@@ -1,7 +1,9 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+DTO (Data Transfer Object):
+DTO là một design pattern dùng để đóng gói và truyền dữ liệu giữa các thành phần/layer trong ứng dụng
+Là một class đơn giản chỉ chứa các thuộc tính (properties) và các phương thức getter/setter
+Không chứa bất kỳ business logic nào
+Mục đích chính là đóng gói dữ liệu để truyền qua network hoặc giữa các layer
  */
 package dto;
 
@@ -10,5 +12,57 @@ package dto;
  * @author tungi
  */
 public class UserDTO {
+    private String userID;
+    private String fullName;
+    private String roleID;
+    private String password;
+
+    public UserDTO() {
+    }
+
+    public UserDTO(String userID, String fullName, String roleID, String password) {
+        this.userID = userID;
+        this.fullName = fullName;
+        this.roleID = roleID;
+        this.password = password;
+    }
+
+    public String getUserID() {
+        return userID;
+    }
+
+    public void setUserID(String userID) {
+        this.userID = userID;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getRoleID() {
+        return roleID;
+    }
+
+    public void setRoleID(String roleID) {
+        this.roleID = roleID;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "UserDTO{" + "userID=" + userID + ", fullName=" + fullName + ", roleID=" + roleID + ", password=" + password + '}';
+    }
+    
     
 }
