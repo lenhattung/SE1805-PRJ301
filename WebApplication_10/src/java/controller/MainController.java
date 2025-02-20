cddfdfbdba3519ff91fbfedd2169f44d32c4a165
Tung Le <lenhattung@gmail.com>
2025-02-20 14:34:35 +0700

    ud
    ud

--- WebApplication_10/src/java/controller/MainController.java
+++ WebApplication_10/src/java/controller/MainController.java
@@ -5,10 +5,13 @@
  */
 package controller;
 
+import dao.BookDAO;
 import dao.UserDAO;
+import dto.BookDTO;
 import dto.UserDTO;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.List;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -52,14 +55,20 @@ public class MainController extends HttpServlet {
                     if (isValidLogin(strUserID, strPassword)) {
                         url = "search.jsp";
                         UserDTO user = getUser(strUserID);
-                        request.setAttribute("user", user);
+                        request.getSession().setAttribute("user", user);
                     } else {
                         request.setAttribute("message", "Incorrect UserID or Password");
                         url = "login.jsp";
                     }
                 } else if (action.equals("logout")) {
-                    request.setAttribute("user", null);
+                    request.getSession().invalidate(); // Hủy session
                     url ="login.jsp";
+                } else if (action.equals("search")) {
+                    url ="search.jsp";
+                    BookDAO bdao = new BookDAO();
+                    String searchTerm = request.getParameter("searchTerm");
+                    List<BookDTO> books = bdao.searchByTitle(searchTerm);
+                    request.setAttribute("books", books);
                 }
             }
         } catch (Exception e) {
